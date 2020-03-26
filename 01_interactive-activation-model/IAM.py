330738d5813a336fd531fa7b5d9f3f0d7f2b1406
kalenkovich <e.kalenkovich@gmail.com>
2020-03-26 11:37:08 +0300

    Listed features of each letter

--- 01_interactive-activation-model/IAM.py
+++ 01_interactive-activation-model/IAM.py
@@ -29,3 +29,45 @@ for i in range(7):
 four_letter_words = kf_corpus[(kf_corpus.word.str.len() == 4) & (kf_corpus.frequency > 4)]
 len(four_letter_words)
 
+
+# # Encode letters as feature bundles 
+
+# In the original model, the input letters came from a simplified font in which each letter is composed of a number of simplified line strokes (*features*) as in the following image:
+# 
+# ![Font from Rumelhar & Siple, 1974](rumelhart-siple-font.jpg)
+
+# Here are all the features numbered from 0 to 13:
+# 
+# ![Numbered line features](line_features.png)
+
+# A list of letters specifying which features it is composed of.
+
+feature_numbers = {
+    'A': [0, 1, 2, 3, 4, 6, 8],
+    'B': [2, 3, 4, 5, 7, 8, 9],
+    'C': [0, 1, 2, 5],
+    'D': [2, 3, 4, 5, 7, 9],
+    'E': [0, 1, 2, 5, 6],
+    'F': [0, 1, 2, 6],
+    'G': [0, 1, 2, 4, 5, 8],
+    'H': [0, 1, 3, 4, 6, 8],
+    'I': [2, 5, 7, 9],
+    'J': [0, 3, 4, 5],
+    'K': [0, 1, 6, 11, 12],
+    'L': [0, 1, 5],
+    'M': [0, 1, 3, 4, 10, 11],
+    'N': [0, 1, 3, 4, 10, 12],
+    'O': [0, 1, 2, 3, 4, 5],
+    'P': [0, 1, 2, 3, 6, 8],
+    'Q': [0, 1, 2, 3, 4, 5, 12],
+    'R': [0, 1, 2, 3, 6, 8, 12],
+    'S': [1, 2, 4, 5, 6, 8],
+    'T': [2, 7, 9],
+    'U': [0, 1, 3, 4, 5],
+    'V': [0, 1, 11, 13],
+    'W': [0, 1, 3, 4, 12, 13],
+    'X': [10, 11, 12, 13],
+    'Y': [9, 10, 11],
+    'Z': [2, 5, 11, 13]
+}
+

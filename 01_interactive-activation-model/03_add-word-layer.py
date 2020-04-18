a3e181dfbf940300e78c33cb35f3fd032eccc9bb
kalenkovich <e.kalenkovich@gmail.com>
2020-04-18 17:55:07 +0300

    Added word-to-word inhibition and word-to-letter excitation

--- 01_interactive-activation-model/03_add-word-layer.py
+++ 01_interactive-activation-model/03_add-word-layer.py
@@ -80,3 +80,95 @@ iam_with_words_1.print_active_letters()
 
 
 # Why is "cork" more activated than "word"?
+
+class IAMWithWords2(IAMWithWords1):
+    def __init__(self):
+        super().__init__()
+        
+        # Word-to-word connections
+        word_to_word_inhibitory = 0.21
+        word_to_word_weights = np.where(
+            np.identity(n_words),
+            0,
+            -word_to_word_inhibitory
+        )
+        word_to_word_connection = Connection(
+            layer_from=self.word_layer,
+            layer_to=self.word_layer,
+            weights=word_to_word_weights
+        )
+        
+        # Word-to-letter        
+        is_excitatory = np.array([[[word[position].lower() == letter.lower()
+                                    for word in corpus.word]
+                                   for letter in alphabet]
+                                  for position in range(4)])
+        is_excitatory = np.reshape(is_excitatory, newshape=(-1, is_excitatory.shape[-1]))
+        
+        word_to_letter_excitatory = 0.30
+        word_to_letter_weights = np.where(
+            is_excitatory.T,
+            word_to_letter_excitatory,
+            0  # There are no inhibitory connections from the word layer to the letter layer
+        )        
+        word_to_letter_connection = Connection(
+            layer_from=self.word_layer,
+            layer_to=self.letter_layer,
+            weights=word_to_letter_weights
+        )
+
+
+iam_with_words_2 = IAMWithWords2()
+iam_with_words_2.present_word('WORK')
+
+
+# Cycle 1
+iam_with_words_2.run_cycle()
+iam_with_words_2.print_active_letters()
+(corpus
+ .assign(activation=iam_with_words_2.word_layer.activations)
+ .sort_values(by='activation', ascending=False)
+)
+
+
+# Cycle 2
+iam_with_words_2.run_cycle()
+iam_with_words_2.print_active_letters()
+(corpus
+ .assign(activation=iam_with_words_2.word_layer.activations)
+ .sort_values(by='activation', ascending=False)
+)
+
+
+# The above is the same as without connection from the word layer because the word layer has just been activated.
+
+# Cycle 3
+iam_with_words_2.run_cycle()
+iam_with_words_2.print_active_letters()
+(corpus
+ .assign(activation=iam_with_words_2.word_layer.activations)
+ .sort_values(by='activation', ascending=False)
+)
+
+
+# All the words have been inhibited somewhat, "work" - less so. Let's run 10  more cycles.
+
+iam_with_words_2.run_n_cycles(10)
+iam_with_words_2.print_active_letters()
+(corpus
+ .assign(activation=iam_with_words_2.word_layer.activations)
+ .sort_values(by='activation', ascending=False)
+)
+
+
+# Now, "work" is much more activated than the other words.
+
+iam_with_words_2.run_n_cycles(10)
+iam_with_words_2.print_active_letters()
+(corpus
+ .assign(activation=iam_with_words_2.word_layer.activations)
+ .sort_values(by='activation', ascending=False)
+)
+
+
+# Now, "work" is the only active word.

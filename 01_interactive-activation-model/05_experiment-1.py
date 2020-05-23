a8cabd442644b59eea8768e3963f515c148a4d68
kalenkovich <e.kalenkovich@gmail.com>
2020-05-23 18:40:56 +0300

    Got the "E" right!
    But had to set resting levels for words at -0.4 - much lower than in the article :-(

--- 01_interactive-activation-model/05_experiment-1.py
+++ 01_interactive-activation-model/05_experiment-1.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-from iam.added_absence_detectors import IAM
+from iam.added_absence_detectors import IAM, corpus
 
 
 iam = IAM()
@@ -40,6 +40,7 @@ def present_letter(iam, word, position):
 def run_bright_trial(iam, word, letter_position=None):
     letter_activations_history = dict(K=[], R = [], D = [], E = [])
     word_activations_history = dict(READ=[], DEAL=[])
+    active_words = set()
     
     def take_snapshot():
         for letter, activation_list in letter_activations_history.items():
@@ -47,6 +48,8 @@ def run_bright_trial(iam, word, letter_position=None):
         
         for word, activation_list in word_activations_history.items():
             activation_list.append(iam.get_word_activation(word))
+            
+        active_words.update(corpus[iam.word_layer.activations > 0].word.tolist())
     
     n_cycles = 40
     n_stim_cycles = 15
@@ -65,12 +68,12 @@ def run_bright_trial(iam, word, letter_position=None):
         iam.run_cycle()
         take_snapshot()
     
-    return letter_activations_history, word_activations_history
+    return letter_activations_history, word_activations_history, active_words
 
 
 # ## Word condition 
 
-letter_activations_history, word_activations_history = run_bright_trial(iam, 'READ', letter_position=None)
+letter_activations_history, word_activations_history, active_words = run_bright_trial(iam, 'READ', letter_position=None)
 
 
 plt.figure(figsize=(10, 6))
@@ -85,11 +88,14 @@ plt.plot(np.array(list(word_activations_history.values())).T)
 plt.legend(list(word_activations_history.keys()), loc='upper left')
 plt.grid()
 plt.yticks(np.arange(-0.2, 1.1, 0.1));
+
+
+print(active_words)
 
 
 # ## Letter with number signs
 
-letter_activations_history, word_activations_history = run_bright_trial(iam, 'READ', letter_position=1)
+letter_activations_history, word_activations_history, active_words = run_bright_trial(iam, 'READ', letter_position=1)
 
 
 plt.figure(figsize=(10, 6))
@@ -105,3 +111,36 @@ plt.legend(list(word_activations_history.keys()), loc='upper left')
 plt.grid()
 plt.yticks(np.arange(-0.2, 1.1, 0.1));
 
+
+print(active_words)
+
+
+# The reason "E" got activate more strongly here than in the article is that more word were activated. In turn, more word were activated because in our model, the resting state is zero.
+
+# ## Letter with number signs - negative resting state of words
+
+iam_negative_resting_state = IAM()
+iam_negative_resting_state.word_layer.resting_activation = -0.4
+
+
+letter_activations_history, word_activations_history, active_words = run_bright_trial(iam_negative_resting_state, 'READ', letter_position=1)
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(np.array(list(letter_activations_history.values())).T)
+plt.legend(list(letter_activations_history.keys()), loc='upper left')
+plt.grid()
+plt.yticks(np.arange(-0.2, 1.1, 0.1));
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(np.array(list(word_activations_history.values())).T)
+plt.legend(list(word_activations_history.keys()), loc='upper left')
+plt.grid()
+plt.yticks(np.arange(-0.2, 1.1, 0.1));
+
+
+print(active_words)
+
+
+# We got the right pattern of activation of "E" but we had to set the resting state to -0.4. In the article, the resting state is between -0.05 and 0.
